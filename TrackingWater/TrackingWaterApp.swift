5837f6aa87b003f046408faf4aa874b67faf14a8
Artem Tarasov <artemdrolllted@gmail.com>
2025-08-04 15:44:45 +0300

    Initial Commit

---
+++ TrackingWater/TrackingWaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackingWaterApp.swift
+//  TrackingWater
+//
+//  Created by Drolllted on 04.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct TrackingWaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
