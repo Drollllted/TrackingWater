8c81a54e1b404ec22e4af25e2e06b5bf3f87456e
Artem Tarasov <artemdrolllted@gmail.com>
2025-08-04 23:12:32 +0300

    Step 1 - setup button go to settingsView

--- TrackingWater/View/InformationView.swift
+++ TrackingWater/View/InformationView.swift
@@ -9,7 +9,26 @@ import SwiftUI
 
 struct InformationView: View {
     var body: some View {
-        
+        NavigationStack{
+            VStack {
+                
+            }
+            .navigationTitle("Information")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    NavigationLink {
+                        SettingsView()
+                    } label: {
+                        Image(systemName: "gear")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 30, height: 30)
+                            .foregroundStyle(.white)
+                    }
+
+                }
+            }
+        }
     }
 }
 
---
+++ TrackingWater/View/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  TrackingWater
+//
+//  Created by Drolllted on 04.08.2025.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    SettingsView()
+}
